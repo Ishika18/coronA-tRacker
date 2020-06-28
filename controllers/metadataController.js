a35a315796613160b332ef63482834b5dba55d87
Shagun <shagunphw@gmail.com>
2020-06-27 18:14:20 -0700

    cleaned up the file a little

--- controllers/metadataController.js
+++ controllers/metadataController.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 metadataController = {
     changeScale: (ratio) => {
-        let api = 'broken-recipe-8369'
+        let api = '<API KEY>'
         let entry = '93d0ce76-f38d-4138-9f52-0857aefe818d'
         let url = `https://console.echoAR.xyz/post?key=${api}&entry=${entry}&data=scale&value=${ratio}`
         fetch(url)
